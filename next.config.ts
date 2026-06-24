0ca24faa7366be727f2db590e87f2158651ed55d
Sanjay Kr Singh <uideveloper09@gmail.com>
2026-06-24 18:21:54 +0530

    Remove dev log from repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.29.173"],
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
